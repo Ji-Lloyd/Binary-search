39435f0139e5dce6a2cee34707a67eb965d94a37
Lloyd <agdanlloyd@gmail.com>
2021-03-23 10:38:26 +0800

    first try using the git here in pycharm

---
+++ mod1.py
@@ -0,0 +1,38 @@
+# module 1 for the main python file
+
+
+# def add(x, y):  # function for adding the two values
+#    return x + y
+
+
+# def sub(x, y):  # function for subtracting the two values
+#    return x - y
+
+
+# def div(x, y):  # function for dividing the two values
+#    return x / y
+
+
+# def mul(x, y):  # function for multiplying the two values
+#    return x * y
+
+# ------------------------------------------------------------------------
+
+# def frst():
+#    print("This function is from ", __name__)
+
+
+# def scnd():
+#    print("This is mod1 2nd function")
+
+
+# def main(): # This is the function that ties together all the functions
+#    frst()
+#    scnd()
+
+
+# if __name__ == '__main__': # If the the user run the program within this python file this statement will execute
+#    main() # The 'main' function is called out if the condition are met
+
+# --------------------------------------------------------------------------------------------------------------------
+print("Hello")
